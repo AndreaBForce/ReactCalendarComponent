0b1a8dc9627d55230ea72b2a79222edfd9e5a933
Diego Moranda <diego.moranda@supsi.ch>
2022-04-06 12:54:34 +0200

    init react app

---
+++ calendar-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/Calendar';
+import Calendar from './components/Calendar';
+
+function App() {
+  return (
+  <Calendar>
+
+  </Calendar>
+  );
+}
+
+export default App;
